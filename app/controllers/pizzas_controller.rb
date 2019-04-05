cbd126c84fefe13d1ddc971605f3a4de72eaf046
Steph Mireault <sdbmireault@gmail.com>
2019-04-05 01:10:57 -0500

    made search more fluid

--- app/controllers/pizzas_controller.rb
+++ app/controllers/pizzas_controller.rb
@@ -3,9 +3,16 @@ class PizzasController < ApplicationController
     @pizzas = Pizza.all.page params[:page]
     @pizza_order = current_order.pizza_orders.new
     @pizzas_old = Pizza.order("name")
+    @query = params[:q]
+    @pizza_search = Pizza.joins(:toppings).where('toppings.name LIKE ?', "%#{@query}%")
   end
 
   def show
     @pizza = Pizza.find(params[:id])
+  end
+
+  def search
+    @query = params[:q]
+    @pizzas = Pizza.joins(:toppings).where('toppings.name LIKE ?', "%#{@query}%")
   end
 end
